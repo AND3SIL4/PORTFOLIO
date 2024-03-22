32651371d9a720b4032257eb4f13b54137be12bd
AND3SIL4 <afsilva6236@misena.edu.co>
2024-03-22 11:17:45 -0500

    style: about component has been modified

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,24 +4,39 @@ import { Lucide, Simple } from "./icons";
 export default () => {
   return (
     <div className="flex flex-col items-start gap-6 lg:gap-8 px-3 lg:px-6 py-4">
+      {/* Introduction  */}
       <div className="flex flex-row items-center gap-4">
-        <Avatar src="/favicon.svg" alt="profile-picture">
+        <Avatar src="https://github.com/AND3SIL4.png" alt="profile-picture">
           <Lucide.IconUser className="size-2/3" />
         </Avatar>
 
         <div className="flex flex-col items-start gap-2">
-          <h1 className="font-medium text-lg lg:text-xl">flamrdevs</h1>
-          <h2 className="font-medium text-2xl lg:text-3xl">UI/UX designer</h2>
+          <h1 className="font-medium text-lg lg:text-xl">Felipe Silva</h1>
+          <h2 className="font-medium text-2xl lg:text-3xl">
+            Software Developer
+          </h2>
         </div>
       </div>
 
+      {/* About  */}
       <div>
-        I am a UI/UX designer from Indonesia, specializing in creating user-centric and visually appealing digital experiences. With a focus
-        on seamless and enjoyable interactions, I aim to enhance the overall user experience through strategic design solutions.
+        I am a{" "}
+        <span className="font-bold text-orange-500">software developer</span>{" "}
+        from <span className="font-bold">Colombia</span>, specializing in
+        creating user-centric software applications giving good digital
+        experiences. With a focus on seamless, enjoyable and useful products, I
+        aim to improve the general user experience and usage through strategic{" "}
+        <span className="font-bold">development solutions</span>.
       </div>
 
-      <div className="flex self-stretch justify-end">
-        <Button as="a" href="https://github.com/flamrdevs/astrolinkt" target="_blank" aria-label="GitHub">
+      {/* GitHub profile */}
+      <div className="flex self-stretch justify-end animate-bounce">
+        <Button
+          as="a"
+          href="https://github.com/AND3SIL4"
+          target="_blank"
+          aria-label="GitHub"
+        >
           <Simple.IconGitHub />
           GitHub
         </Button>
