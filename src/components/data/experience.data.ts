60541ba11c17fecce68854e0f32502132e0ee794
AND3SIL4 <afsilva6236@misena.edu.co>
2024-05-14 19:17:08 -0500

    fix(app): experience title fixed

--- src/components/data/experience.data.ts
+++ src/components/data/experience.data.ts
@@ -8,7 +8,7 @@ export const ExperienceData: ExperienceInterface[] = [
       "Responsible for reporting tools for the company directors, using different tools such as Google Locker Studio and databases, delivering a user-friendly and comfortable product.",
   },
   {
-    title: "Desarrollador Aplicaciones",
+    title: "Applications Developer",
     date: "December 18, 2023 - Currently...",
     description:
       "Responsible for optimizing processes and the value chain with the aim of being more agile and sustainable in the future, according to commercial intelligence.",
